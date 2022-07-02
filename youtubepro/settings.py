b260087a4d1b3633e02a3f0b129e32075efa5512
itzm416 <itzrahul6449@gmail.com>
2022-07-02 18:32:47 +0530

    project

--- youtubepro/settings.py
+++ youtubepro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['itz-youtube-clone.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['shielded-springs-23226.herokuapp.com','localhost']
 
 
 # Application definition
